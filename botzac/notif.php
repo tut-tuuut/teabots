f2a619189f39a3ce3a8990a25c813a4d45e87042
Johan Poirier <johan.poirier@gmail.com>
2016-03-01 09:58:50 +0100

    Add: room parameter

--- botzac/notif.php
+++ botzac/notif.php
@@ -6,22 +6,26 @@ require_once(__DIR__ . '/../data.php');
 require_once(__DIR__ . '/quotes.php');
 
 // room
-$room = 'dev';
+if (sizeof($argv) >= 2) {
+  parse_str($argv[1]);
+} else {
+  die('room is mandatory');
+}
 
 $ch = curl_init('https://tea-ebook.hipchat.com/v2/room/' . $rooms[$room]['id'] . '/notification?auth_token=' . $rooms[$room]['token']);
 $data = array(
-    "color" => $colors[array_rand($colors)],
-    "message" => $quotes[array_rand($quotes)],
-    "notify" => true,
-    "message_format" => "text"
+  "color" => $colors[array_rand($colors)],
+  "message" => $quotes[array_rand($quotes)],
+  "notify" => true,
+  "message_format" => "text"
 );
 $dataJson = json_encode($data);
 
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch, CURLOPT_POSTFIELDS, $dataJson);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    'Content-Type: application/json',
-    'Content-Length: ' . strlen($dataJson))
+  'Content-Type: application/json',
+  'Content-Length: ' . strlen($dataJson))
 );
 
 curl_exec($ch);
