433991c18b34f2ef9e3e1f6382af35a39d03f19b
Johan Poirier <johan.poirier@gmail.com>
2016-03-02 17:05:59 +0100

    Add: biscotte quote

--- botzac/quotes.php
+++ botzac/quotes.php
@@ -13,4 +13,5 @@ $quotes = [
     "On a en tout autour du bide des serveurs.",
     "C'est la foire d'empoigne !",
     "Ils savent faire de l'informatique comme moi je parle serbo-croate du nord.",
+    "Faudrait pas qu'il se gratte tout seul la biscotte dans son coin."
 ];
