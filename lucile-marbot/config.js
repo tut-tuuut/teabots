192c0b26bff499dac9ad49dfaae6bde1364a68a3
Johan Poirier <johan.poirier@gmail.com>
2016-03-25 13:55:15 +0100

    Add: lucile marbot

---
+++ lucile-marbot/config.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const config = {
+  'hipchat_key': '',
+  'hipchat_room': 0,
+  'hipchat_bot': 'Bot',
+  'twitter_consumer_key': '',
+  'twitter_consumer_secret': '',
+  'twitter_access_token': '',
+  'twitter_access_token_secret': '',
+  'twitter_keywords': []
+};
+
+module.exports = config;
---
+++ lucile-marbot/index.js
@@ -0,0 +1,29 @@
+'use strict'
+const config = require(__dirname + '/config.test.js');
+const Twit = require('twit');
+const HC = require('node-hipchat');
+
+let twitter = new Twit({
+  consumer_key: config['twitter_consumer_key'],
+  consumer_secret: config['twitter_consumer_secret'],
+  access_token: config['twitter_access_token'],
+  access_token_secret: config['twitter_access_token_secret'],
+  timeout_ms: 60*1000  // optional HTTP request timeout to apply to all requests.
+});
+
+let hipchat = new HC(config['hipchat_key']);
+const params = {
+  room: config['hipchat_room'],
+  from: config['hipchat_bot']
+};
+
+// Listen to TEA twitter accounts
+let stream = twitter.stream('statuses/filter', { track: config['twitter_keywords'] });
+
+console.log('Listening to Twitter, tracking ' + config['twitter_keywords'].join(', ') + '.');
+stream.on('tweet', function (tweet) {
+  // posting to HipChat
+  let content = params;
+  content.message = tweet.text;
+  hipchat.postMessage(params, null);
+});
