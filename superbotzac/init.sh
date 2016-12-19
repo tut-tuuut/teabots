9d7568c2ba01c17188be1233337006e769073978
Johan Poirier <johan.poirier@gmail.com>
2016-12-19 13:28:27 +0100

    custom french analyzer

--- superbotzac/init.sh
+++ superbotzac/init.sh
@@ -1,11 +1,44 @@
 #!/usr/bin/env bash
 
-curl -XPUT 'localhost:9200/bigbrother?pretty' -d'
+curl -XPUT 'localhost:9200/bigbrother' -d'
 {
-    "settings": {
-        "number_of_shards" :   1,
-        "number_of_replicas" : 0
+  "settings": {
+    "number_of_shards" :   1,
+    "number_of_replicas" : 0,
+    "analysis": {
+      "filter": {
+        "french_elision": {
+          "type": "elision",
+          "articles_case": true,
+          "articles": [
+            "l", "m", "t", "qu", "n", "s",
+            "j", "d", "c", "jusqu", "quoiqu",
+            "lorsqu", "puisqu"
+          ]
+        },
+        "french_stop": {
+          "type": "stop",
+          "stopwords": "stopwords/french.txt"
+        },
+        "french_stemmer": {
+          "type": "stemmer",
+          "language": "light_french"
+        }
+      },
+      "analyzer": {
+        "custom_french": {
+          "tokenizer":  "standard",
+          "filter": [
+            "french_elision",
+            "lowercase",
+            "french_stop",
+            "french_keywords",
+            "french_stemmer"
+          ]
+        }
+      }
     }
+  }
 }'
 
 curl -XPUT 'localhost:9200/bigbrother/_mapping/tweet?pretty' -d'
@@ -17,7 +50,7 @@ curl -XPUT 'localhost:9200/bigbrother/_mapping/tweet?pretty' -d'
     },
     "message": {
       "type": "string",
-      "analyzer": "french"
+      "analyzer": "custom_french"
     },
     "room": {
       "type": "string",
