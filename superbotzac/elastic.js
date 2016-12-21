f4a3c3f00014ce424c2e021bc4a4e7faf455df88
Johan Poirier <johan.poirier@gmail.com>
2016-12-21 09:07:11 +0100

    bucket agg by username

--- superbotzac/elastic.js
+++ superbotzac/elastic.js
@@ -104,7 +104,7 @@ module.exports = {
         "aggs": {
           "top-chatters": {
             "terms": {
-              "field": "author"
+              "field": "username"
             }
           }
         }
