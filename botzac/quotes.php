47d8e0ec608e3948a26cb96b5d1a1ab85665a49b
Johan Poirier <johan.poirier@gmail.com>
2018-12-14 11:04:26 +0100

    Add quote

--- botzac/quotes.php
+++ botzac/quotes.php
@@ -57,5 +57,6 @@ $quotes = [
     'Roulé a la main sous l‘SSL !',
     'En tout cas, y’a une esbrouffe là-dessous.',
     'Là si ça marche, je me fais monter une statue dans l’open space.',
-    'Je vous emmerde... mais en toute cordialité.',
+    'Je vous emmerde… mais en toute cordialité.',
+    'Là c’était pas un coup d’oeil, c’était un coup de pied !',
 ];
